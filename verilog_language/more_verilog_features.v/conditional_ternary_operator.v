5a69300eb6aa0e5a4d4fefd05e6b4f02b10f4d93
SAMBATHKUMAR S <131866247+sambathsaga@users.noreply.github.com>
2026-07-09 18:57:17 +0530

    Create conditional_ternary_operator.v

---
+++ verilog_language/more_verilog_features.v/conditional_ternary_operator.v
@@ -0,0 +1,9 @@
+module top_module (
+    input [7:0] a, b, c, d,
+    output [7:0] min);
+    wire [7:0]x,y;
+    assign x=(a<b)?a:b;
+    assign y=(c<d)?c:d;
+    assign min=(x<y)?x:y;
+
+endmodule
